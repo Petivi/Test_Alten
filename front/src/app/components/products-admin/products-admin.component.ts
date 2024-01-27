053ba3764c0db6fcd12cfe83344666c25760b0bb
Petivi <33059675+Petivi@users.noreply.github.com>
2024-01-27 23:03:58 +0100

    crud admin

--- front/src/app/components/products-admin/products-admin.component.ts
+++ front/src/app/components/products-admin/products-admin.component.ts
@@ -7,6 +7,10 @@ import { InputTextModule  } from 'primeng/inputtext';
 import { DropdownModule } from 'primeng/dropdown';
 import { TagModule } from 'primeng/tag';
 import { MultiSelectModule } from 'primeng/multiselect';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 import { ProductsService } from '../../services/products.service';
 import { Product } from '../../models/product';
@@ -14,7 +18,8 @@ import { Product } from '../../models/product';
 @Component({
   selector: 'app-products-admin',
   standalone: true,
-  imports: [CommonModule, TableModule, InputTextModule, DropdownModule, TagModule, MultiSelectModule, FormsModule],
+  imports: [CommonModule, TableModule, InputTextModule, DropdownModule, TagModule, MultiSelectModule, FormsModule, ToolbarModule, DialogModule, ConfirmDialogModule],
+  providers:[ConfirmationService, MessageService],
   templateUrl: './products-admin.component.html',
   styleUrl: './products-admin.component.css'
 })
@@ -22,10 +27,18 @@ export class ProductsAdminComponent {
   @ViewChild('dt') dt: Table | undefined;
   protected tab_products: Array<Product> = [];
   protected tab_products2: Array<Product> = [];
-  clonedProducts: { [s: string]: Product; } = {};
+  protected clonedProducts: { [s: string]: Product; } = {};
+
+  protected new_product: any = {};
+  protected selectedProducts: Array<Product> = [];
+  protected submitted: boolean = false;
+  protected productDialog: boolean = false;
+  protected statuses: any[] = [];
 
   constructor(
     private _productsService: ProductsService,
+    private _confirmationService: ConfirmationService,
+    private _messageService: MessageService,
   ){}
 
   ngOnInit(){
@@ -36,6 +49,85 @@ export class ProductsAdminComponent {
   applyFilterGlobal(event: any, stringVal: any) { 
     this.dt?.filterGlobal((event.target as HTMLInputElement).value, stringVal); 
   }
+
+
+  openNew() {
+    this.new_product = {};
+    this.submitted = false;
+    this.productDialog = true;
+}
+
+deleteSelectedProducts() {
+  this._confirmationService.confirm({
+      message: 'Are you sure you want to delete the selected products?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+          this.tab_products = this.tab_products.filter(val => !this.selectedProducts.includes(val));
+          this.selectedProducts = [];
+      }
+  });
+}
+
+deleteProduct(product: Product) {
+  this._confirmationService.confirm({
+      message: 'Are you sure you want to delete ' + product.name + '?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+          this.tab_products = this.tab_products.filter(val => val.id !== product.id);
+          this.new_product = {};
+          this._messageService.add({severity:'success', summary: 'Successful', detail: 'Product Deleted', life: 3000});
+      }
+  });
+}
+
+hideDialog() {
+  this.productDialog = false;
+  this.submitted = false;
+}
+
+saveProduct() {
+  this.submitted = true;
+
+  if (this.new_product.name.trim()) {
+      if (this.new_product.id) {
+          this.tab_products[this.findIndexById(this.new_product.id)] = this.new_product;
+          this._messageService.add({severity:'success', summary: 'Successful', detail: 'Product Updated', life: 3000});
+      }
+      else {
+          this.new_product.id = this.createId();
+          this.new_product.image = 'product-placeholder.svg';
+          this.tab_products.push(this.new_product);
+          this._messageService.add({severity:'success', summary: 'Successful', detail: 'Product Created', life: 3000});
+      }
+
+      this.tab_products = [...this.tab_products];
+      this.productDialog = false;
+      this.new_product = {};
+  }
+}
+
+findIndexById(id: number): number {
+  let index = -1;
+  for (let i = 0; i < this.tab_products.length; i++) {
+      if (this.tab_products[i].id === id) {
+          index = i;
+          break;
+      }
+  }
+
+  return index;
+}
+
+createId(): string {
+  let id = '';
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for ( var i = 0; i < 5; i++ ) {
+      id += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return id;
+}
 
 
   onRowEditInit(product: Product) {
